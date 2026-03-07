20422a964af14df11d19e09f9ee0ff57df1140ae
leoabvleo <leoabvleo@gmail.com>
2026-03-07 13:34:02 -0300

    Limpeza de arquivos ignorados do repositório

--- query_db.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-from models import Transacao
-import sqlalchemy as sa
-with app.app_context():
-    anos = db.session.query(sa.extract('year', Transacao.data), db.func.count(Transacao.id)).group_by(sa.extract('year', Transacao.data)).all()
-    print("Transacoes", anos)
